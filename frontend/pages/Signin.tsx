9051eae3cdefd5e80c5b4b6a5343272a7913ce96
smeet96 <smeetsherki@gmail.com>
2025-09-11 19:37:04 +0530

    changes and added features

--- frontend/pages/Signin.tsx
+++ frontend/pages/Signin.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
+
 export const Signin = (): React.JSX.Element => {
- 
+ const navigate = useNavigate()
+ const [email , setEmail] = useState()
+ const [password , setPassword] = useState()
   return (
+    console.log(email , password) ,
     <section className="bg-white dark:bg-white">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -12,9 +19,21 @@ export const Signin = (): React.JSX.Element => {
                   Sign in to your account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
-                   <Input placeholder="john@gmail.com"  label="email" type="email"/>
-                   <Input placeholder="......"  label="Passowrd" type="password"/>
-                   <Button button="sign in" />
+                   <Input placeholder="john@gmail.com"  label="email" type="email" onChange={(e:any) => {setEmail(e.target.value)}} />
+                   <Input placeholder="......"  label="Passowrd" type="password" onChange={(e:any)=> {setPassword(e.target.value)}}/>
+                   <Button button="sign in" onClick={async ()=>{
+                    try {
+                        const response = await axios.post("http://localhost:3000/api/v1/user/signin" , {
+                        email,
+                        password
+                    })
+                    localStorage.setItem("token" , response.data.token)
+                    navigate("/dashboard")
+                    } catch (error) {
+                        return alert("error while signing up")
+                    }
+                   }} />
+                   <Button onClick={()=> {localStorage.removeItem("token")}} button="log out" />
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don't have an account? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Signup here</a>
                   </p>
