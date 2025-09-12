77826cf182fa29b5453d6feed31c947d96530b99
smeet96 <smeetsherki@gmail.com>
2025-09-12 15:35:09 +0530

    added dashboard

--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,7 +1,7 @@
 export const  Input = ({placeholder , label , type , onChange}:any) => {
     return (
          <div>
-                      <label   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+                      <label   className="block mb-2 text-sm font-medium text-gray-900 dark:text-pink-600">{label}</label>
                       <input type={type} onChange={onChange}   placeholder={placeholder} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
          </div>
     )
